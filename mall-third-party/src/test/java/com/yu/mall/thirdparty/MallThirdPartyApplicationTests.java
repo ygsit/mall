740bcdb6c952ddad696f5d64db35c95c2ed69fd3
ygsit <183213557@qq.com>
2022-01-08 19:59:36 +0800

    access-key删除

--- mall-third-party/src/test/java/com/yu/mall/thirdparty/MallThirdPartyApplicationTests.java
+++ mall-third-party/src/test/java/com/yu/mall/thirdparty/MallThirdPartyApplicationTests.java
@@ -21,14 +21,7 @@ class MallThirdPartyApplicationTests {
 
 	@Test
 	public void testUpload() throws FileNotFoundException {
-        /*// Endpoint以杭州为例，其它Region请按实际情况填写。
-        String endpoint = "http://oss-cn-shanghai.aliyuncs.com";
-        // 云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，创建并使用RAM子账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建。
-        String accessKeyId = "LTAI4G92k9RWazUYuuu7gCkZ";
-        String accessKeySecret = "CITkpNcUvXfzaRa5rYZxzJi12kXVCw";
 
-        // 创建OSSClient实例。
-        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);*/
 
 		// 上传文件流。
 		InputStream inputStream = new FileInputStream("D:\\Picture\\Pictures\\4.jpg");
